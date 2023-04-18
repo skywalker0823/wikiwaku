94cf384b2561812538d76b96bac6216c7aa44a47
eddie <eddie@lacoal.net>
2023-04-18 11:47:12 +0800

    update line messages function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import base64
 import functions_framework
 from fastapi import FastAPI, Request
+import requests, json, dotenv, os
+
+dotenv.load_dotenv()
 
 app = FastAPI()
 
@@ -9,8 +12,21 @@ app = FastAPI()
 def hello_pubsub(cloud_event):
     # Print out the data from Pub/Sub, to prove that it worked
     print("!!! Active !!!")
-    print("test 123")
-    print(base64.b64decode(cloud_event.data["message"]["data"]))
+    print("Sending Line update message")
+    data = {
+        "to": os.getenv("User_ID"),
+        "messages": [
+            {
+                "type": "text",
+                "text": "功能測試 by 軒"
+            }
+        ]
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": os.getenv("Channel_Access_Token")
+    }
+    r = requests.post("https://api.line.me/v2/bot/message/push", data=json.dumps(data), headers=headers)
 
 # Triggered from a message from line user post request
 @app.post("/my-path")
@@ -18,3 +34,4 @@ async def hello_http(request: Request):
     print("!!! User-ask !!!")
     data = await request.json()
     print(data)
+
