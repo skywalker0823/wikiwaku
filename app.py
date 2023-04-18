42039786ad5e0fd7994002797039e7eced066a23
eddie <eddie@lacoal.net>
2023-04-18 09:55:58 +0800

    test build

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import functions_framework
 def hello_pubsub(cloud_event):
     # Print out the data from Pub/Sub, to prove that it worked
     print("!!! Active !!!")
+    print("test 123")
     print(base64.b64decode(cloud_event.data["message"]["data"]))
 
 # Triggered from a message from line user
