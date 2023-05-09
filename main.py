7a263eaad60116eacc57e268cb0c39c7928e0c8a
eddie <eddie@lacoal.net>
2023-05-09 13:19:54 +0800

    Nasa bug fixed

--- main.py
+++ main.py
@@ -15,21 +15,21 @@ openai.api_key = os.getenv('OPEN_AI_API_KEY')
 # Wiki API section
 @functions_framework.cloud_event
 def hello_pubsub(cloud_event):
-    print("!!! Active !!!")
     today = datetime.datetime.now()
     date = today.strftime('%m/%d')
+    print("!!! Active !!! data date: ", date)
     text = ""
     url = f'https://api.wikimedia.org/feed/v1/wikipedia/zh/onthisday/selected/{date}'
     wiki_headers = {
         'Authorization': f'Bearer {WIKI_TOKEN}',
         'User-Agent': WIKI_MAIL
     }
+    print("getting wiki data...")
     wiki_response = requests.get(url, headers=wiki_headers)
     if wiki_response.status_code != 200:
         print("Error broadcasting message: ", wiki_response.status_code, wiki_response.text)
-    
+    print("wiki data received...")
     response = wiki_response.json().get('selected')
-
     data_set = {"messages": []}
     for i in response:
         year = i['pages'][0]['title']
@@ -50,6 +50,7 @@ def hello_pubsub(cloud_event):
 
     # NASA API section
     nasa_url = f'https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}'
+    print("getting NASA data...")
     nasa_response = requests.get(nasa_url)
     nasa_image_set = None
     nasa_text_set = None
@@ -58,6 +59,7 @@ def hello_pubsub(cloud_event):
         nasa_data = nasa_response.json()
         nasa_image = nasa_data['url']
         nasa_explanation = nasa_data['explanation']
+        print("translating to Triditional Chinese...")
         translate_response = openai.Completion.create(
             model = "text-davinci-003",
             prompt = f"Translate this to Triditional Chinese: \n\n{nasa_explanation}\n\n",
@@ -80,7 +82,9 @@ def hello_pubsub(cloud_event):
         "Content-Type": "application/json",
         "Authorization": f"Bearer {CHANNEL_ACCESS_TOKEN}"
     }
-
+    print("all data set...broadcasting to Line...")
+    # print(today_date_info, data_set, nasa_image_set, nasa_text_set)
+    # Line broadcast section
     today_r = requests.post("https://api.line.me/v2/bot/message/broadcast", data=json.dumps(today_date_info), headers=headers)
     r = requests.post("https://api.line.me/v2/bot/message/broadcast", data=json.dumps(data_set), headers=headers)
     nasa_image_r = requests.post("https://api.line.me/v2/bot/message/broadcast", data=json.dumps(nasa_image_set), headers=headers)
@@ -119,3 +123,6 @@ async def line_webhook(request: Request):
         print('Webhook error')
         return 'Webhook error'
     
+
+# if __name__ == '__main__':
+#     hello_pubsub(cloud_event=None)
