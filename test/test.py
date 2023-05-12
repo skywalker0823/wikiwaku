fca9e5de600bb5f03fba825a27ec418c713e9cd3
eddie <eddie@lacoal.net>
2023-05-12 18:16:16 +0800

    adding test

---
+++ test/test.py
@@ -0,0 +1,23 @@
+# pytest test.py
+
+import json
+from fastapi.testclient import TestClient
+from ..main import app
+
+client = TestClient(app)
+
+def test_line_webhook():
+    payload = {
+        "events": [
+            {
+                "type": "message",
+                "message": {
+                    "text": "test"
+                }
+
+    }
+        ]
+    }
+    response = client.post("/", json.dumps(payload))
+    assert response.status_code == 200
+    assert response.text == 'OK'
